962bb8ddd259876e3802c63b0f71dd5a7f31f0db
Deepika Negi <deep08negi@gmail.com>
2026-01-11 11:41:04 +0530

    added .env to protect api_key

---
+++ yt_video_stats.py
@@ -0,0 +1,32 @@
+import requests
+import json
+
+import os
+from dotenv import  load_dotenv
+
+load_dotenv(dotenv_path="./.env")
+api_key = os.getenv("API_KEY")
+CHANNEL_HANDLE="MrBeast"
+
+def get_playlist_id():
+    
+    try:
+
+        url=f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={api_key}"
+        response = requests.get(url)
+
+        # print(response)
+        response.raise_for_status
+        data=response.json()
+        # print(json.dumps(data, indent=4))
+
+        channel_items=data["items"][0]
+
+        channel_playlist_id=channel_items["contentDetails"]["relatedPlaylists"]["uploads"]
+        print(channel_playlist_id)
+        return channel_playlist_id
+    except requests.exceptions.RequestException as e:
+        raise e
+
+if __name__ == "__main__":
+    get_playlist_id()
